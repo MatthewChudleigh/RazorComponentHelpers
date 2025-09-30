92db1a015d8e2cf9e2f57a5786c36419a4bd53b5
matthew-chudleigh <matthew.chudleigh@entaingroup.com.au>
2025-10-01 09:16:41 +1000

    Add the SSE events and ToAsyncEnumerable helpers

---
+++ src/RazorComponentHelpers/ServerEvents.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+
+namespace RazorComponentHelpers;
+
+public static class ServerEvents
+{
+    public record Event(string Name, string Data);
+
+    public static Event Html(string name, string html) => new(name, html.ReplaceLineEndings(""));
+    public static Event Json<T>(string name, T data) => new(name, JsonSerializer.Serialize(data));
+    
+    public static async Task Stream(this HttpContext context, IAsyncEnumerable<Event> events, CancellationToken cancel)
+    {
+        context.Response.Headers.ContentType = "text/event-stream";
+        context.Response.Headers.CacheControl = "no-cache";
+        context.Response.Headers.Connection = "keep-alive";
+
+        await foreach (var e in events.WithCancellation(cancel))
+        {
+            await context.Response.WriteAsync($"event: {e.Name}\n", cancellationToken: cancel);
+            await context.Response.WriteAsync($"data: {e.Data}\n\n", cancellationToken: cancel);
+            await context.Response.Body.FlushAsync(cancel);
+        }
+    }
+}
