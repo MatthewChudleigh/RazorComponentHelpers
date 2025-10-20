5374e4d4d843ac62160679bcf34093b472d086c3
Matthew Chudleigh <matthewtoday@gmail.com>
2025-10-20 13:32:26 +1000

    Allow rendering a RenderFragment within a Layout as the Body

--- src/RazorComponentHelpers/ComponentRenderer.cs
+++ src/RazorComponentHelpers/ComponentRenderer.cs
@@ -9,18 +9,37 @@ using Microsoft.AspNetCore.Components.Web;
 
 public class ComponentRenderer(IServiceProvider serviceProvider, ILoggerFactory loggerFactory)
 {
+    public async Task<IResult> RenderPageAsync<TLayout>(RenderFragment fragment)
+        where TLayout : IComponent
+    {
+        var inner = await RenderFragmentAsync(fragment);
+        return await RenderLayoutWithContentAsync<TLayout>(inner);
+    }
+    
     public async Task<IResult> RenderPageAsync<TComponent, TLayout>(Dictionary<string, object?>? parameters = null)
         where TComponent : IComponent
         where TLayout : IComponent
     {
-        return await InnerRenderLayoutWithBodyAsync<TComponent, TLayout>(null, parameters);
+        var inner = await InnerRenderComponentAsync<TComponent>(null, parameters);
+        return await RenderLayoutWithContentAsync<TLayout>(inner);
     }
 
     public async Task<IResult> RenderPageAsync<TComponent, TLayout>(CancellationToken cancel, Dictionary<string, object?>? parameters = null) 
         where TComponent : IComponent 
         where TLayout : IComponent
     {
-        return await InnerRenderLayoutWithBodyAsync<TComponent, TLayout>(cancel, parameters);
+        var inner = await InnerRenderComponentAsync<TComponent>(cancel, parameters);
+        return await RenderLayoutWithContentAsync<TLayout>(inner);
+    }
+    
+    public Task<string> RenderComponentAsync<T>(Dictionary<string, object?>? parameters = null) where T : IComponent
+    {
+        return InnerRenderComponentAsync<T>(null, parameters);
+    }
+
+    public Task<string> RenderComponentAsync<T>(CancellationToken cancel, Dictionary<string, object?>? parameters = null) where T : IComponent
+    {
+        return InnerRenderComponentAsync<T>(cancel, parameters);
     }
 
     public async Task<string> RenderComponentWithLayoutAsync<TComponent, TLayout>(Dictionary<string, object?>? parameters = null)
@@ -37,35 +56,47 @@ public class ComponentRenderer(IServiceProvider serviceProvider, ILoggerFactory 
         return await InnerRenderLayoutWithChildContentAsync<TComponent, TLayout>(cancel, parameters);
     }
     
-    public Task<string> RenderComponentAsync<T>(Dictionary<string, object?>? parameters = null) where T : IComponent
-    {
-        return InnerRenderComponentAsync<T>(null, parameters);
-    }
-
-    public Task<string> RenderComponentAsync<T>(CancellationToken cancel, Dictionary<string, object?>? parameters = null) where T : IComponent
-    {
-        return InnerRenderComponentAsync<T>(cancel, parameters);
-    }
-
-    private async Task<string> InnerRenderComponentAsync<T>(CancellationToken? cancel, Dictionary<string, object?>? parameters = null) where T : IComponent
+    public async Task<string> RenderFragmentAsync(RenderFragment fragment)
     {
         await using var htmlRenderer = new HtmlRenderer(serviceProvider, loggerFactory);
 
-        parameters ??= [];
-        if (cancel != null)
+        var parameters = new Dictionary<string, object?>
         {
-            parameters.Add("Cancel", cancel);
-        }
+            ["Fragment"] = fragment
+        };
 
         var componentParameters = ParameterView.FromDictionary(parameters);
         var output = await htmlRenderer.Dispatcher.InvokeAsync(async () =>
         {
-            var result = await htmlRenderer.RenderComponentAsync<T>(componentParameters);
+            var result = await htmlRenderer.RenderComponentAsync<RenderFragmentWrapper>(componentParameters);
             return result.ToHtmlString();
         });
         return output;
     }
     
+    public async Task<IResult> RenderLayoutWithContentAsync<TLayout>(string htmlContent, string section = "Body")
+        where TLayout : IComponent
+    {
+        await using var htmlRenderer = new HtmlRenderer(serviceProvider, loggerFactory);
+
+        // Render layout with htmlContent as section (default: "Body")
+        var layoutParams = new Dictionary<string, object?>
+        {
+            [section] = (RenderFragment)(builder =>
+            {
+                builder.AddMarkupContent(0, htmlContent);
+            })
+        };
+
+        var layoutParameterView = ParameterView.FromDictionary(layoutParams);
+
+        var html = await htmlRenderer.Dispatcher.InvokeAsync(
+            async () => (await htmlRenderer.RenderComponentAsync<TLayout>(layoutParameterView)).ToHtmlString()
+        );
+
+        return Results.Text(html, "text/html");
+    }
+
     private async Task<string> InnerRenderLayoutWithChildContentAsync<TChildComponent, TLayout>(
         CancellationToken? cancel,
         Dictionary<string, object?>? parameters = null)
@@ -86,47 +117,20 @@ public class ComponentRenderer(IServiceProvider serviceProvider, ILoggerFactory 
         });
     }
     
-    private async Task<IResult> InnerRenderLayoutWithBodyAsync<T, TLayout>(
-        CancellationToken? cancel,
-        Dictionary<string, object?>? parameters = null)
-        where T : IComponent
-        where TLayout : IComponent
+    private async Task<string> InnerRenderComponentAsync<T>(CancellationToken? cancel, Dictionary<string, object?>? parameters = null) where T : IComponent
     {
         await using var htmlRenderer = new HtmlRenderer(serviceProvider, loggerFactory);
 
-        var inner = await InnerRenderComponentAsync<T>(cancel, parameters);
-
-        // Render layout with inner component as "Body"
-        var layoutParams = new Dictionary<string, object?>
+        parameters ??= [];
+        if (cancel != null)
         {
-            ["Body"] = (RenderFragment)(builder =>
-            {
-                builder.AddMarkupContent(0, inner);
-            })
-        };
-
-        var layoutParameterView = ParameterView.FromDictionary(layoutParams);
-
-        var html = await htmlRenderer.Dispatcher.InvokeAsync(
-            async () => (await htmlRenderer.RenderComponentAsync<TLayout>(layoutParameterView)).ToHtmlString()
-        );
-
-        return Results.Text(html, "text/html");
-    }
-
-    public async Task<string> RenderFragmentAsync(RenderFragment fragment)
-    {
-        await using var htmlRenderer = new HtmlRenderer(serviceProvider, loggerFactory);
-
-        var parameters = new Dictionary<string, object?>
-        {
-            ["Fragment"] = fragment
-        };
+            parameters.Add("Cancel", cancel);
+        }
 
         var componentParameters = ParameterView.FromDictionary(parameters);
         var output = await htmlRenderer.Dispatcher.InvokeAsync(async () =>
         {
-            var result = await htmlRenderer.RenderComponentAsync<RenderFragmentWrapper>(componentParameters);
+            var result = await htmlRenderer.RenderComponentAsync<T>(componentParameters);
             return result.ToHtmlString();
         });
         return output;
