e1e529389a0e8e9d6a67397cff65011b143482bd
matthew-chudleigh <matthewtoday@gmail.com>
2025-11-10 07:53:44 +1000

    Allow multiple data elements in an SSE event

--- src/RazorComponentHelpers/ServerEvents.cs
+++ src/RazorComponentHelpers/ServerEvents.cs
@@ -3,14 +3,17 @@ using Microsoft.AspNetCore.Http;
 
 namespace RazorComponentHelpers;
 
+public record SseEvent(string Name, Func<IEnumerable<string>> Data);
+
 public static class ServerEvents
 {
-    public record Event(string Name, string Data);
-
-    public static Event Html(string name, string html) => new(name, html.ReplaceLineEndings(""));
-    public static Event Json<T>(string name, T data) => new(name, JsonSerializer.Serialize(data));
+    public static SseEvent Data(string name, Func<IEnumerable<string>> data) => new(name, data);
+    public static SseEvent Html(string name, string html) => new(name, () => [html]);
+    public static SseEvent Html(string name, IEnumerable<string> html) => new(name, () => html.Select(h => h));
+    public static SseEvent Json<T>(string name, T data) => new(name, () => [JsonSerializer.Serialize(data)]);
+    public static SseEvent Json<T>(string name, IEnumerable<T> data) => new(name, () => data.Select(d => JsonSerializer.Serialize(d)));
     
-    public static async Task Stream(this HttpContext context, IAsyncEnumerable<Event> events, CancellationToken cancel)
+    public static async Task Stream(this HttpContext context, IAsyncEnumerable<SseEvent> events, CancellationToken cancel)
     {
         context.Response.Headers.ContentType = "text/event-stream";
         context.Response.Headers.CacheControl = "no-cache";
@@ -19,7 +22,12 @@ public static class ServerEvents
         await foreach (var e in events.WithCancellation(cancel))
         {
             await context.Response.WriteAsync($"event: {e.Name}\n", cancellationToken: cancel);
-            await context.Response.WriteAsync($"data: {e.Data}\n\n", cancellationToken: cancel);
+            foreach (var d in e.Data())
+            {
+                await context.Response.WriteAsync($"data: {d.ReplaceLineEndings("")}\n", cancellationToken: cancel);
+            }
+
+            await context.Response.WriteAsync($"\n", cancellationToken: cancel);
             await context.Response.Body.FlushAsync(cancel);
         }
     }
